88c96afa1669b8f56ae16e61afb7e7979da1eb1f
Karmen <karmenmelvan56@gmail.com>
2025-03-26 21:23:07 +0100

    cetvrti zadatak

---
+++ zadatakk4_vjezba1.py
@@ -0,0 +1,16 @@
+def jednadžba_pravca(x1, y1, x2, y2):
+    koeficijent_smjera=round((y2-y1)/(x2-x1))
+    c=(y1-koeficijent_smjera*x1)
+    print(f'Jednadžba pravca je: y= {koeficijent_smjera}x+ {c}')
+x1=int(input()) # recimo neka budu koordinate cijeli brojevi
+x2=int(input())
+x3=int(input())
+x4=int(input())
+jednadžba_pravca(x1, y1, x2, y2)
+
+
+
+
+
+
+

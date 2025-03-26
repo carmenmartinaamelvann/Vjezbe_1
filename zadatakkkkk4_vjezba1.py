f1226c42d860394e8abac86153e4619089260efb
Karmen <karmenmelvan56@gmail.com>
2025-03-26 21:35:50 +0100

    4 zad

--- zadatakkkkk4_vjezba1.py
+++ zadatakkkkk4_vjezba1.py
@@ -2,10 +2,10 @@ def jednadžba_pravca(x1, y1, x2, y2):
     koeficijent_smjera=round((y2-y1)/(x2-x1))
     c=(y1-koeficijent_smjera*x1)
     print(f'Jednadžba pravca je: y= {koeficijent_smjera}x+ {c}')
-x1=int(input()) # recimo neka budu koordinate cijeli brojevi.
+x1=int(input()) # recimo neka budu koordinate cijeli brojevi...
+y1=int(input())
 x2=int(input())
-x3=int(input())
-x4=int(input())
+y2=int(input())
 jednadžba_pravca(x1, y1, x2, y2)
 
 
